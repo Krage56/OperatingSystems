534b5f365d2045718fe8847ade152b5881f3ee81
Проскуряков Иван <ivan.proskuryakov.2001@yandex.ru>
2021-09-25 14:01:17 +0300

    Ready for testing in server

--- lab1/main.c
+++ lab1/main.c
@@ -2,14 +2,34 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 void exit_handler()
 {
-    printf("The process %d is died!\n", getpid());
+    printf("The process %d is died! He was a child of %d!\n", getpid(), getppid());
 }
 int main()
 {
     atexit(exit_handler);
-    printf("I'm living in my own!\n");
-    sleep(10);
+    pid_t pid;
+    switch (pid = fork())
+    {
+    case -1:
+        perror("fork"); /* произошла ошибка */
+        exit(1);        /*выход из родительского процесса*/
+    case 0:
+        printf(" CHILD: Это процесс-потомок!\n");
+        printf(" CHILD: Мой PID -- %d\n", getpid());
+        printf(" CHILD: PID моего родителя -- %d\n", getppid());
+        sleep(10);
+        exit(1);
+    default:
+        printf("PARENT: Это процесс-родитель!\n");
+        printf("PARENT: Мой PID -- %d\n", getpid());
+        printf("PARENT: PID моего потомка %d\n", pid);
+        printf("PARENT: Я жду, пока потомок не вызовет exit()...\n");
+        int status = -1;
+        wait(&status); /*Дождёмся выхода из любого дочернего процесса - он всего один*/
+    }
     return 0;
 }

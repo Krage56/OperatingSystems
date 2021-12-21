ee55dd98c5e60073770a20bc6778e996ef65c1b3
Проскуряков Иван <ivan.proskuryakov.2001@yandex.ru>
2021-12-21 18:21:37 +0300

    lab5 working ver

---
+++ lab5/main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <time.h>
+
+#include <sys/shm.h>
+#include <errno.h>
+#define true 1
+
+pthread_mutex_t mutex;
+
+unsigned long long count = 0;
+
+void *reader_handler(void *args)
+{
+    while (count < 77)
+    {
+        pthread_mutex_lock(&mutex);
+        printf("My TID is %lu, count = %llu\n", (pthread_self()), count);
+        pthread_mutex_unlock(&mutex);
+        sleep(2);
+    }
+    return NULL;
+}
+
+void *writer_handler(void *args)
+{
+    while (count < 77)
+    {
+        pthread_mutex_lock(&mutex);
+        ++count;
+        pthread_mutex_unlock(&mutex);
+        sleep(1);
+    }
+    return NULL;
+}
+
+int main()
+{
+    const int readres_num = 10, writers_num = 1;
+    pthread_t readers[readres_num], writers[writers_num];
+    void *(*reader)(void *) = reader_handler;
+    void *(*writer)(void *) = writer_handler;
+    for (int i = 0; i < readres_num; ++i)
+    {
+        pthread_create(&(readers[i]), NULL, reader, NULL);
+    }
+    for (int i = 0; i < writers_num; ++i)
+    {
+        pthread_create(&(writers[i]), NULL, writer, NULL);
+    }
+    for (int i = 0; i < writers_num; ++i)
+    {
+        pthread_join(writers[i], NULL);
+    }
+    for (int i = 0; i < readres_num; ++i)
+    {
+        pthread_join(readers[i], NULL);
+    }
+
+    pthread_mutex_init(&mutex, NULL);
+
+    pthread_mutex_destroy(&mutex);
+    return 0;
+}
